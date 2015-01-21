3a9114b4bbe765630ada5d6345ed4fddb6975334
klauspost <klauspost@gmail.com>
2015-01-21 13:11:47 +0100

    Add web-api streaming example.

--- examples/readme.md
+++ examples/readme.md
@@ -1,18 +1,20 @@
 
 Installing dependencies:
 
-go get github.com/klauspost/dawa/examples/...
+```go get github.com/klauspost/dawa/examples/...```
 
 
 Running web example:
+```
 go run webapi.go
-
+go run webapi-stream.go
+```
 Run file import examples:
 
 1) Unpack files in examples.7z
 
 2) Run the file you would like to test:
-
+```
 go run adgangsadresser-csv.go
 go run adgangsadresser-json.go
 go run adgangs-adresser-json.go
@@ -20,3 +22,4 @@ go run adresser-csv.go
 go run adresser-json.go
 go run postnumre-json.go
 go run vejstykker-json.go
+```
---
+++ examples/webapi-stream.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"github.com/klauspost/dawa"
+	"io"
+	"log"
+	"net/http"
+	"time"
+)
+
+// Makes a request to the web api and prints the result.
+func main() {
+	// Get an address
+	resp, err := http.Get("http://dawa.aws.dk/adgangsadresser?husnr=14&postnr=9000")
+
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	// Send the response stream to a decoder
+	iter, err := dawa.ImportAdgangsAdresserJSON(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	// Read responses one by one
+	n := 0
+	t := time.Now()
+	for {
+		a, err := iter.Next()
+		if err == io.EOF {
+			log.Printf("Finished reading %d entries in %v.\n", n, time.Now().Sub(t))
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		n++
+		fmt.Printf("Entry:%#v\n", a)
+	}
+}
